462591d7e42bbe1fbd838c72a722647601fb2ac5
ngatti68 <norberto.o.gatti@gmail.com>
2022-10-10 20:37:26 -0300

    Commit con nuevo componente
    Se agregó el componente experiencia

--- Portfolio_FrontEnd_Angular/src/app/app.module.ts
+++ Portfolio_FrontEnd_Angular/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     BannerComponent,
-    AcercaDeComponent
+    AcercaDeComponent,
+    ExperienciaComponent
   ],
   imports: [
     BrowserModule
